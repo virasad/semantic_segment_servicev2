22f3fb6b66a96a7258e6cefd626ac0f0a45acb39
Amir Sharifi <am.sharifi1995@gmail.com>
2022-07-01 16:38:08 +0430

    :sparkles: feat(docker): handle exceptions #13

--- inference/main.py
+++ inference/main.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI, File, UploadFile
 from starlette.responses import StreamingResponse
 
 from predict import InferenceSeg
+
 # import imantics
 
 
@@ -34,7 +35,7 @@ def root():
 @app.post("/predict")
 async def predict(image: UploadFile = File(...), return_image: bool = False, return_coco: bool = False,
                   ):
-    # try:
+    try:
         if return_image and return_coco:
             raise ValueError('return_image and return_coco cannot be True at the same time')
 
@@ -46,27 +47,26 @@ async def predict(image: UploadFile = File(...), return_image: bool = False, ret
             return StreamingResponse(io.BytesIO(response), media_type='image/jpeg')
 
         elif return_coco:
-            print('yesss')
-            # m, classes = response
             return {"results": response}
         return {"message": "Success"}
 
-    # except Exception as e:
-    #     return {"message": str(e)}
 
-@app.post("/n_classes")
+    except Exception as e:
+        return {"message": str(e)}
+
+@app.post("/n-classes")
 async def set_classes(n_classes: int):
     detector.set_classes(n_classes)
     return {"message": "Success"}
 
 
-@app.post("/set_size")
+@app.post("/set-size")
 async def set_size(weight: int, height: int):
     detector.set_size(weight, height)
     return {"message": "Success"}
 
 
-@app.post("/set_model")
+@app.post("/set-model")
 async def set_model(model_path: str):
     detector.set_model(model_path)
     return {"message": "Success"}
